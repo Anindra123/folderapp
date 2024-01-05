aeb146db77f12af927242fa096f051f9f8dc0506
Anindra123 <bivasdas911@gmail.com>
2024-01-05 20:53:41 +0600

    removed unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -236,7 +236,7 @@ function App() {
 
     }
 
-    //setSortOption("ascending");
+
   }
 
   function sortFolderDescending() {
@@ -289,16 +289,7 @@ function App() {
     //setSortOption("descending");
   }
 
-  // function handleSort() {
-  //   if (sortOption === "ascending") {
-  //     console.log(sortOption);
-  //     sortFolderAscending();
-  //   }
-  //   if (sortOption === "descending") {
-  //     console.log(sortOption);
-  //     sortFolderDescending();
-  //   }
-  // }
+
 
   function handleSortOption(e: string) {
     if (sortType === "Name") {
@@ -309,103 +300,6 @@ function App() {
         sortFolderDescending();
       }
     }
-    //setSortOption(() => e);
-    // if (sortType === "Name") {
-
-    //   if ( sortOption === "ascending") {
-    //     setSortOption("ascending");
-    //     // const temp_folders = { ...folders };
-
-    //     // const sortable_keys: Array<{ id: string, name: string }> = [];
-    //     // let current_folders = temp_folders;
-    //     // if (path.length > 0) {
-
-    //     //   path.forEach(p => {
-    //     //     current_folders.children[p.id] = current_folders.children[p.id] || {}
-    //     //     current_folders = current_folders.children[p.id]
-    //     //   })
-
-    //     //   Object.keys(current_folders.children).forEach((f) => {
-    //     //     sortable_keys.push({ id: f, name: current_folders.children[f].name });
-    //     //   })
-    //     //   sortable_keys.sort((a, b) => compareFunctionAsc(a, b))
-    //     //   let temp_child = { ...current_folders.children };
-    //     //   temp_child = {}
-    //     //   sortable_keys.forEach(val => {
-    //     //     temp_child[val.id] = current_folders.children[val.id]
-    //     //   })
-    //     //   current_folders.children = temp_child;
-
-    //     //   setRenderFolder(current_folders);
-    //     //   setFolders(temp_folders);
-
-    //     // } else {
-    //     //   Object.keys(temp_folders.children).forEach((f) => {
-    //     //     sortable_keys.push({ id: f, name: temp_folders.children[f].name });
-    //     //   })
-    //     //   sortable_keys.sort((a, b) => compareFunctionAsc(a, b))
-    //     //   let temp_child = { ...temp_folders.children };
-    //     //   temp_child = {}
-    //     //   sortable_keys.forEach(val => {
-    //     //     temp_child[val.id] = temp_folders.children[val.id]
-    //     //   })
-    //     //   temp_folders.children = temp_child;
-
-    //     //   setRenderFolder(temp_folders);
-    //     //   setFolders(temp_folders);
-
-
-    //     // }
-    //     // setSortOption("ascending");
-    //     sortFolderAscending();
-    //   }
-    //   if ( sortOption === "descending") {
-    //     // const temp_folders = { ...folders };
-    //     // const sortable_keys: Array<{ id: string, name: string }> = [];
-    //     // let current_folders = temp_folders;
-    //     // if (path.length > 0) {
-
-    //     //   path.forEach(p => {
-    //     //     current_folders.children[p.id] = current_folders.children[p.id] || {}
-    //     //     current_folders = current_folders.children[p.id]
-    //     //   })
-
-    //     //   Object.keys(current_folders.children).forEach((f) => {
-    //     //     sortable_keys.push({ id: f, name: current_folders.children[f].name });
-    //     //   })
-    //     //   sortable_keys.sort((a, b) => compareFunctionDesc(a, b))
-    //     //   let temp_child = { ...current_folders.children };
-    //     //   temp_child = {}
-    //     //   sortable_keys.forEach(val => {
-    //     //     temp_child[val.id] = current_folders.children[val.id]
-    //     //   })
-    //     //   current_folders.children = temp_child;
-    //     //   setRenderFolder(current_folders);
-    //     //   setFolders(temp_folders);
-
-    //     // } else {
-    //     //   Object.keys(temp_folders.children).forEach((f) => {
-    //     //     sortable_keys.push({ id: f, name: temp_folders.children[f].name });
-    //     //   })
-    //     //   sortable_keys.sort((a, b) => compareFunctionDesc(a, b))
-    //     //   let temp_child = { ...temp_folders.children };
-    //     //   temp_child = {}
-    //     //   sortable_keys.forEach(val => {
-    //     //     temp_child[val.id] = temp_folders.children[val.id]
-    //     //   })
-    //     //   temp_folders.children = temp_child;
-
-    //     //   setRenderFolder(temp_folders);
-    //     //   setFolders(temp_folders);
-
-    //     // }
-    //     // setSortOption("descending");
-    //     sortFolderDescending();
-
-    //   }
-    // }
-    //localStorage.setItem("all_folder", JSON.stringify(folders));
-    //handleSort();
   }
 
 
